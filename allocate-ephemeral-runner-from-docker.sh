fc1a95852f86df7a856035d9399f30173c04871b
Alex <alex.boden@uwaterloo.ca>
2024-07-08 01:21:18 +0000

    improved comments

--- allocate-ephemeral-runner-from-docker.sh
+++ allocate-ephemeral-runner-from-docker.sh
@@ -8,9 +8,8 @@ REMOVAL_TOKEN=$3
 LABELS=$4
 
 # start docker
-echo "Starting Docker on Slurm"
+echo "INFO Starting Docker on Slurm"
 slurm-start-dockerd.sh
-echo "Docker started"
 
 export DOCKER_HOST=unix:///tmp/run/docker.sock
 
@@ -20,49 +19,40 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Define the parent directory for GitHub Actions
+echo "INFO Docker in Slurm started"
+
+# Define the parent directory for GitHub Actions in the host machine
 PARENT_DIR="/tmp/runner_$SLURM_JOB_ID"
-
-# Define the GitHub Actions working directory
 GITHUB_ACTIONS_WKDIR="$PARENT_DIR/_work"
-
-# Create the parent directory if it doesn't exist
 mkdir -p $PARENT_DIR
-
-# Add permissions to the parent directory
 chown -R $(id -u):$(id -g) $PARENT_DIR
 chmod -R 777 $PARENT_DIR
 
 
+# Start the actions runner container. Mount the docker socket and the parent directory (of the working directory).
 DOCKER_CONTAINER_ID=$(docker run -d --name "ghar_$SLURM_JOB_ID" --mount type=bind,source=/tmp/run/docker.sock,target=/var/run/docker.sock --mount type=bind,source=$PARENT_DIR,target=$PARENT_DIR ghcr.io/watonomous/actions-runner-image:main tail -f /dev/null)
-
 docker exec $DOCKER_CONTAINER_ID /bin/bash -c "sudo chmod 666 /var/run/docker.sock" # Allows the runner to access the docker socket
+# Configure the permissions of the parent directory within the container
 docker exec $DOCKER_CONTAINER_ID /bin/bash -c "mkdir \"$PARENT_DIR\"" 
 docker exec $DOCKER_CONTAINER_ID /bin/bash -c "sudo chown -R runner:runner \"$PARENT_DIR\""
-# docker exec $DOCKER_CONTAINER_ID /bin/bash -c "echo permissions for \"$PARENT_DIR\""
-# docker exec $DOCKER_CONTAINER_ID /bin/bash -c "ls -ld \"$PARENT_DIR\""
 docker exec $DOCKER_CONTAINER_ID /bin/bash -c "sudo chmod -R 755 \"$PARENT_DIR\"" 
 
-
 # Execute commands in the container to register, run one job, and then remove the runner
-echo "Registering runner..."
+echo "INFO Registering runner..."
 
-# docker exec $DOCKER_CONTAINER_ID /bin/bash -c "./config.sh --url \"$REPO_URL\" --token \"$REGISTRATION_TOKEN\" --labels \"$LABELS\" --name \"slurm_$SLURM_JOB_ID\" --unattended --ephemeral"
 docker exec $DOCKER_CONTAINER_ID /bin/bash -c "/home/runner/config.sh --work "$GITHUB_ACTIONS_WKDIR" --url \"$REPO_URL\" --token \"$REGISTRATION_TOKEN\" --labels \"$LABELS\" --name \"slurm_$SLURM_JOB_ID\" --unattended --ephemeral"
 
-echo "Running runner..."
+echo "INFO Running runner..."
 docker exec $DOCKER_CONTAINER_ID /bin/bash -c "/home/runner/run.sh"
 
 docker exec $DOCKER_CONTAINER_ID /bin/bash -c "ls" 
 
-echo "Removing runner..."
+echo "INFO Removing runner..."
 docker exec $DOCKER_CONTAINER_ID /bin/bash -c "/home/runner/config.sh remove --token $REMOVAL_TOKEN"
 
 docker stop $DOCKER_CONTAINER_ID
 docker rm $DOCKER_CONTAINER_ID
 
-echo "Docker container removed"
-echo "Script finished"
-
-echo "Exiting Docker in Docker"
+echo "INFO Docker container removed"
+echo "INFO allocate-ephemeral-runner-from-docker.sh finished, now exiting."
 exit
