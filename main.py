7ff01fca12fe48436997948f98ad3f951b256e63
Alex Boden <arboden@uwaterloo.ca>
2025-02-02 18:03:35 -0500

    add log

--- main.py
+++ main.py
@@ -217,7 +217,7 @@ def allocate_actions_runner(job_id, token):
         # sbatch resource allocation command
         command = [
             "sbatch",
-            # f"--nodelist=thor-slurm1",
+			f"--output=/var/log/slurm-ci/slurm-ci-%j.log",
             f"--job-name=slurm-{runner_size_label}-{job_id}",
             f"--mem-per-cpu={runner_resources['mem-per-cpu']}",
             f"--cpus-per-task={runner_resources['cpu']}",
