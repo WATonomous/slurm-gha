ee882e2f25b4ded643549f79687c506759fa330e
Alex Boden <arboden@uwaterloo.ca>
2025-05-11 16:31:07 -0400

    add custom running jobs

--- runner_size_config.py
+++ runner_size_config.py
@@ -14,5 +14,25 @@ def get_runner_resources(runner_label):
         return {"cpu" : 4, "mem-per-cpu" : "2G", "tmpdisk" : 16384, "time" : "00:30:00"}
     elif runner_label == "slurm-runner-xlarge":
         return {"cpu" : 16, "mem-per-cpu" : "2G", "tmpdisk" : 16384, "time" : "00:30:00"}
+    elif runner_label == "slurm-runner-medium-long-running":
+        return {"cpu" : 4, "mem-per-cpu" : "2G", "tmpdisk" : 16384, "time" : "06:00:00"}
+    elif runner_label.startswith("slurm-runner-"):
+        # format like slurm-runner-4cpu-2mempercpu-30:00time-16384tmpdisk
+        # default values if not found
+        parts = runner_label.split("-")
+        resources = parts[2:] # remove slurm-runner-
+        cpu, mem_per_cpu, time, tmpdisk = 2, 2, "30:00", 16384
+        for part in resources:
+            if part.endswith("cpu"):
+                cpu = int(part[:-3])
+            elif part.endswith("mempercpu"):
+                mem_per_cpu = int(part[:-8])
+            elif part.endswith("time"):
+                time = int(part[:-4])
+            elif part.endswith("tmpdisk"):
+                tmpdisk = int(part[:-7])
+            else:
+                raise ValueError(f"Unknown resource type {part} in runner label {runner_label}.")
+        return {"cpu" : cpu, "mem-per-cpu" : f"{mem_per_cpu}G", "tmpdisk" : tmpdisk, "time" : f"{time}"}
     else:
         raise ValueError(f"Runner label {runner_label} not found.")
