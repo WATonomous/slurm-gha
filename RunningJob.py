a76bdadcb8c415de367f83295098427f2074f564
Alex Boden <arboden@uwaterloo.ca>
2025-05-11 16:31:30 -0400

    add repo to running job

--- RunningJob.py
+++ RunningJob.py
@@ -1,8 +1,9 @@
 from typing import List
 
 class RunningJob:
-    def __init__(self, job_id: int, slurm_job_id: int, workflow_name: str, job_name: str, labels: List[str]):
+    def __init__(self, repo: str, job_id: int, slurm_job_id: int, workflow_name: str, job_name: str, labels: List[str]):
         """Class to represent a running Github Actions Job on Slurm."""
+        self.repo = repo
         self.job_id = job_id
         self.slurm_job_id = slurm_job_id
         self.workflow_name = workflow_name
